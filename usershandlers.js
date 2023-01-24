65ef27349f6c53777f35882bebbe99fca8a029ea
MORTREUX Christophe <7610671u@commun.ad.sncf.fr>
2023-01-24 12:14:31 +0100

    Add express 4 putUser

--- usershandlers.js
+++ usershandlers.js
@@ -64,6 +64,7 @@ const users = [
           res.status(500).send("Error retrieving data from database");
         });
     };
+    //Quête express 03
     const postUser = (req, res) => {
       const { firstname, lastname, email, city, language } = req.body;
       database
@@ -81,8 +82,32 @@ const users = [
         });
     };
 
+     //Quête express 04
+     const updateUser = (req, res) => {
+      const id = parseInt(req.params.id);
+      const { firstname, lastname, email, city, language } = req.body;
+    
+      database
+        .query(
+          "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+          [firstname, lastname, email, city, language, id]
+        )
+        .then(([result]) => {
+          if (result.affectedRows === 0) {
+            res.status(404).send("Not Found");
+          } else {
+            res.sendStatus(204);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send("Error editing the user");
+        });
+    };
+
   module.exports = {
       getUsers,
       getUsersById,
-      postUser,
+      postUser,  //Quête express 03
+      updateUser,//Quête express 04
     };
